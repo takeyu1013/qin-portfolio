b12f63fa6c3303039b6449fba40ebeb8be6e4072
takeyu1013 <yuto1013@gmail.com>
2022-08-31 22:46:20 +0900

    ♻️ Refactor the blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,7 +4,6 @@ import type { MicroCMSListResponse } from "microcms-js-sdk";
 import type { Blog } from "src/components/blogs";
 
 import {
-  Box,
   Center,
   Divider,
   Group,
@@ -17,13 +16,14 @@ import useSWRInfinite from "swr/infinite";
 import useInfiniteScroll from "react-infinite-scroll-hook";
 
 import { Blogs } from "src/components/blogs";
-import { client } from "src/lib/client";
 import { useMediaQuery } from "src/lib/mantine";
+import { client } from "src/lib/client";
 
 type Props = MicroCMSListResponse<Blog>;
 
 const Blog: NextPage<Props> = ({ contents }) => {
   const largerThanXs = useMediaQuery("sm");
+  const { colors } = useMantineTheme();
   const { data, size, setSize, error } = useSWRInfinite<
     MicroCMSListResponse<Blog>
   >(
@@ -37,7 +37,6 @@ const Blog: NextPage<Props> = ({ contents }) => {
   );
   const loading = !error && !data;
   const hasNextPage = data === undefined || size * 10 < data[0].totalCount;
-  const { colors } = useMantineTheme();
   const [sentryRef] = useInfiniteScroll({
     loading,
     hasNextPage,
@@ -49,44 +48,41 @@ const Blog: NextPage<Props> = ({ contents }) => {
 
   return (
     <Group position="center" grow>
-      <Box
+      <Stack
+        spacing={24}
         px={16}
         py={40}
         className="max-w-5xl"
         style={{ minHeight: largerThanXs ? 638 : 596 }}
       >
-        <Stack spacing={24}>
-          <Title order={2}>Blog</Title>
-          <Divider />
-          {data ? (
-            <Stack spacing={24}>
-              {data.map(({ contents }, index) => {
-                return (
-                  <Blogs
-                    key={index}
-                    size={contents.length}
-                    contents={contents}
-                  />
-                );
-              })}
-              {(loading || hasNextPage) && (
-                <Center ref={sentryRef}>
-                  <Loader color={colors.pink[6]} />
-                </Center>
-              )}
-            </Stack>
-          ) : (
-            <Blogs size={10} contents={contents} />
-          )}
-        </Stack>
-      </Box>
+        <Title order={2}>Blog</Title>
+        <Divider />
+        {data ? (
+          <Stack spacing={24}>
+            {data.map(({ contents }, index) => {
+              return (
+                <Blogs key={index} size={contents.length} contents={contents} />
+              );
+            })}
+            {(loading || hasNextPage) && (
+              <Center ref={sentryRef}>
+                <Loader color={colors.pink[6]} />
+              </Center>
+            )}
+          </Stack>
+        ) : (
+          <Blogs size={10} contents={contents} />
+        )}
+      </Stack>
     </Group>
   );
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
+  const props = await client.getList<Blog>({ endpoint: "blog" });
+
   return {
-    props: await client.getList<Blog>({ endpoint: "blog" }),
+    props,
   };
 };
 
