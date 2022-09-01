9e346d8b6251c87fc87cdd281c2cb17d7a12bef1
takeyu1013 <yuto1013@gmail.com>
2022-09-01 21:18:18 +0900

    🐛 Fix 404 in the blog detail page

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,10 +3,13 @@ import type { MicroCMSListResponse } from "microcms-js-sdk";
 
 import type { Blog } from "src/components/blogs";
 
+import { useRouter } from "next/router";
 import {
   Box,
+  Center,
   Divider,
   Group,
+  Loader,
   Stack,
   Text,
   Title,
@@ -17,42 +20,62 @@ import dayjs from "dayjs";
 
 import { useMediaQuery } from "src/lib/mantine";
 import { client } from "src/lib/client";
+import useSWR from "swr";
 
 type Props = MicroCMSListResponse<Blog>["contents"][number];
 
-const BlogId: NextPage<Props> = ({ title, body, publishedAt }) => {
+const BlogId: NextPage<Props> = (props) => {
   const { colors } = useMantineTheme();
   const largerThanXs = useMediaQuery("sm");
+  const router = useRouter();
+  const { id } = router.query;
+  const { data } = useSWR<Props>(id ? `/api/blog/${id}` : null, async (url) =>
+    (await fetch(url)).json()
+  );
+
+  if (!data && !props.title) {
+    return (
+      <Box style={{ minHeight: largerThanXs ? 926 : 596 }}>
+        <Group position="center">
+          <Stack px={16} py={40} className="max-w-5xl">
+            <Center>
+              <Loader color={colors.pink[6]} />
+            </Center>
+          </Stack>
+        </Group>
+      </Box>
+    );
+  }
+  const { title, body, publishedAt } = data || props;
 
   return (
     <Group position="center" grow>
-      <Box
+      <Stack
+        spacing={20}
         px={16}
         py={40}
         className="max-w-5xl"
         style={{ minHeight: largerThanXs ? 926 : 596 }}
       >
-        <Stack spacing={20}>
-          <Title order={2} className="leading-10">
-            {title}
-          </Title>
-          <Divider />
-          <Stack spacing={8}>
-            <Text
-              component="time"
-              dateTime={publishedAt}
-              size="xs"
-              weight={700}
-              color={colors.dark[2]}
-            >
-              {dayjs(publishedAt).format("YYYY.MM.DD")}
-            </Text>
-            <TypographyStylesProvider>
-              <div dangerouslySetInnerHTML={{ __html: body }} />
-            </TypographyStylesProvider>
-          </Stack>
+        <Title order={2} className="leading-10">
+          {title}
+        </Title>
+        <Divider />
+        <Stack spacing={8}>
+          <Text
+            component="time"
+            dateTime={publishedAt}
+            size="xs"
+            weight={700}
+            color={colors.dark[2]}
+          >
+            {dayjs(publishedAt).format("YYYY.MM.DD")}
+          </Text>
+          <TypographyStylesProvider>
+            <div dangerouslySetInnerHTML={{ __html: body }} />
+          </TypographyStylesProvider>
         </Stack>
-      </Box>
+      </Stack>
     </Group>
   );
 };
@@ -63,7 +86,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
