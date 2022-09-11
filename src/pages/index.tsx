f5b6f1e60ff14b1f6d67d60b5fd6e2d16c34b87d
takeyu1013 <yuto1013@gmail.com>
2022-09-11 13:20:56 +0900

    💄 Change the style and size of tweets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -226,31 +226,40 @@ const Home: NextPage<Props> = ({ blogs, portfolios }) => {
             <Title order={2}>Twitter</Title>
             <Divider />
             {data ? (
-              data.tweets.map(({ created_at, html }, index) => {
+              data.tweets.slice(0, 3).map(({ id, created_at, html }, index) => {
                 return (
-                  <Group key={index} py={16} noWrap className="items-start">
-                    <Avatar
-                      size={38}
-                      radius="xl"
-                      src={data.user?.profile_image_url}
-                    />
-                    <Stack spacing={4}>
-                      <Group spacing={8}>
-                        <Title order={5}>{data.user?.name}</Title>
-                        <Text size="xs" color={colors.dark[2]} weight={700}>
-                          @{data.user?.username}・
-                          {dayjs(created_at).format("M月D日")}
-                        </Text>
-                      </Group>
-                      <TypographyStylesProvider>
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: html,
-                          }}
-                        />
-                      </TypographyStylesProvider>
-                    </Stack>
-                  </Group>
+                  <Anchor
+                    key={index}
+                    href={`https://twitter.com/${data.user?.username}/status/${id}`}
+                    variant="text"
+                    target="_blank"
+                  >
+                    <Group py={16} noWrap className="items-start">
+                      <Avatar
+                        size={38}
+                        radius="xl"
+                        src={data.user?.profile_image_url}
+                      />
+                      <Stack spacing={4}>
+                        <Group spacing={8}>
+                          <Title order={5}>{data.user?.name}</Title>
+                          <Text size="xs" color={colors.dark[2]} weight={700}>
+                            @{data.user?.username}・
+                            {dayjs(created_at).format("M月D日")}
+                          </Text>
+                        </Group>
+                        <TypographyStylesProvider>
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: html
+                                .split(`<blockquote class="twitter-tweet">`)[1]
+                                .split("&mdash;")[0],
+                            }}
+                          />
+                        </TypographyStylesProvider>
+                      </Stack>
+                    </Group>
+                  </Anchor>
                 );
               })
             ) : (
