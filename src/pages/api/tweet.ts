c66edbd44649228cd082e689aeb51168111bd3bb
takeyu1013 <yuto1013@gmail.com>
2022-09-14 23:20:23 +0900

    ♻️ Refactor Twitter API related code

--- src/pages/api/tweet.ts
+++ src/pages/api/tweet.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type {
   TwitterResponse,
   usersIdTweets,
-  findUserByUsername,
 } from "twitter-api-sdk/dist/types";
 
 import { Client } from "twitter-api-sdk";
@@ -10,7 +9,10 @@ import { Client } from "twitter-api-sdk";
 const handler = async (
   _req: NextApiRequest,
   res: NextApiResponse<{
-    user: TwitterResponse<findUserByUsername>["data"];
+    user: Exclude<
+      Exclude<TwitterResponse<usersIdTweets>["includes"], undefined>["users"],
+      undefined
+    >[number];
     tweets: (Exclude<
       TwitterResponse<usersIdTweets>["data"],
       undefined
@@ -18,24 +20,31 @@ const handler = async (
   }>
 ) => {
   const client = new Client(process.env.BEARER_TOKEN);
-  const { data: user } = await client.users.findUserByUsername("shimabu_it", {
-    "user.fields": ["profile_image_url"],
-  });
-  if (!user) {
+  const { data, includes } = await client.tweets.usersIdTweets(
+    "1199992312815734784",
+    {
+      expansions: ["author_id"],
+      "tweet.fields": ["created_at"],
+      "user.fields": ["profile_image_url"],
+      exclude: ["replies", "retweets"],
+    }
+  );
+  if (!data) {
     return;
   }
-  const { data: tweets } = await client.tweets.usersIdTweets(user.id, {
-    "tweet.fields": ["created_at"],
-    exclude: ["replies", "retweets"],
-  });
-  if (!tweets) {
+  if (!includes || !includes.users || !includes.users[0]) {
     return;
   }
-  const embedTweets = await Promise.all(
-    tweets.map(async (tweet) => {
+  const user = includes.users[0];
+  const tweets = await Promise.all(
+    data.map(async (tweet) => {
       const { id } = tweet;
-      const url = `https://publish.twitter.com/oembed?url=https://twitter.com/${user.username}/status/${id}`;
-      const data = await fetch(url);
+      if (!includes.users) {
+        return { ...tweet, html: "" };
+      }
+      const data = await fetch(
+        `https://publish.twitter.com/oembed?url=https://twitter.com/${includes.users[0].username}/status/${id}`
+      );
       const { html } = await data.json();
       if (typeof html !== "string") {
         return { ...tweet, html: "" };
@@ -43,7 +52,7 @@ const handler = async (
       return { ...tweet, html };
     })
   );
-  res.status(200).json({ user, tweets: embedTweets });
+  res.status(200).json({ user, tweets });
 };
 
 export default handler;
