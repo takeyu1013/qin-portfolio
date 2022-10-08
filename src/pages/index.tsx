73585f788b6c79e45e0ed913c1764208e65302a0
takeyu1013 <yuto1013@gmail.com>
2022-10-08 21:19:32 +0900

    🚚 Rename Twitter data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,7 +79,7 @@ const Home: NextPage<Props> = ({ blogs, portfolios }) => {
   const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
   const fetcher = async (url: string) => (await fetch(url)).json();
-  const { data } = useSWR<{
+  const { data: twitter } = useSWR<{
     tweets: (Exclude<
       TwitterResponse<usersIdTweets>["data"],
       undefined
@@ -275,44 +275,48 @@ const Home: NextPage<Props> = ({ blogs, portfolios }) => {
           <Stack px={16} spacing={24}>
             <Title order={2}>Twitter</Title>
             <Divider />
-            {data ? (
-              data.tweets.slice(0, 3).map(({ id, created_at, html }, index) => {
-                return (
-                  <Anchor
-                    key={index}
-                    href={`https://twitter.com/${data.user?.username}/status/${id}`}
-                    variant="text"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Group py={16} noWrap className="items-start">
-                      <Avatar
-                        size={38}
-                        radius="xl"
-                        src={data.user?.profile_image_url}
-                      />
-                      <Stack spacing={4}>
-                        <Group spacing={8}>
-                          <Title order={5}>{data.user?.name}</Title>
-                          <Text size="xs" color={colors.dark[2]} weight={700}>
-                            @{data.user?.username}・
-                            {dayjs(created_at).format("M月D日")}
-                          </Text>
-                        </Group>
-                        <TypographyStylesProvider>
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html: html
-                                .split(`<blockquote class="twitter-tweet">`)[1]
-                                .split("&mdash;")[0],
-                            }}
-                          />
-                        </TypographyStylesProvider>
-                      </Stack>
-                    </Group>
-                  </Anchor>
-                );
-              })
+            {twitter ? (
+              twitter.tweets
+                .slice(0, 3)
+                .map(({ id, created_at, html }, index) => {
+                  return (
+                    <Anchor
+                      key={index}
+                      href={`https://twitter.com/${twitter.user?.username}/status/${id}`}
+                      variant="text"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Group py={16} noWrap className="items-start">
+                        <Avatar
+                          size={38}
+                          radius="xl"
+                          src={twitter.user?.profile_image_url}
+                        />
+                        <Stack spacing={4}>
+                          <Group spacing={8}>
+                            <Title order={5}>{twitter.user?.name}</Title>
+                            <Text size="xs" color={colors.dark[2]} weight={700}>
+                              @{twitter.user?.username}・
+                              {dayjs(created_at).format("M月D日")}
+                            </Text>
+                          </Group>
+                          <TypographyStylesProvider>
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: html
+                                  .split(
+                                    `<blockquote class="twitter-tweet">`
+                                  )[1]
+                                  .split("&mdash;")[0],
+                              }}
+                            />
+                          </TypographyStylesProvider>
+                        </Stack>
+                      </Group>
+                    </Anchor>
+                  );
+                })
             ) : (
               <Center>
                 <Loader color={colors.pink[6]} />
@@ -324,7 +328,7 @@ const Home: NextPage<Props> = ({ blogs, portfolios }) => {
                 variant={dark ? "white" : "filled"}
                 radius="xl"
                 component="a"
-                href={`https://twitter.com/${data?.user?.username}`}
+                href={`https://twitter.com/${twitter?.user?.username}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
